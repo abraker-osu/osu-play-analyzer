d70d40363627497fdb4a4306d40a29c71b05c0b2
abraker <abraker95@gmail.com>
2022-01-06 22:17:17 -0500

    Attempt to fix bpm and bpm changes processing releases and holds

--- app/data_recording/data_processing.py
+++ app/data_recording/data_processing.py
@@ -98,25 +98,55 @@ class DataProcessing():
         map_y = score_data['map_y'].values
         map_t = score_data['map_t'].values
 
+        act_type = score_data['action'].values  # What action was performed (press, release, hold)
+        scr_type = score_data['type'].values    # What the resultant score action was (hit press, hit release, miss, empty, etc)
+        
+        press_select = (act_type == StdScoreData.ACTION_PRESS)
+        empty_filter = (scr_type != StdScoreData.TYPE_EMPTY)
+        
+        note_start_select = press_select & empty_filter
+        note_start_idx_ref = np.arange(note_start_select.shape[0])[note_start_select]
+        not_empty_idx_ref = np.arange(empty_filter.shape[0])[empty_filter]
+
         def __get_note_dt():
             """
             Gets the time interal between each note
-            """
-            dt = np.zeros(map_t.shape[0])
+            Data that does not pertain to a valid entries is marked as np.nan
             
-            dt[0]  = np.inf
-            dt[1:] = map_t[1:] - map_t[:-1]  # t1 - t0
+            Entries consist of score points related to press, release, etc. 
+            An entry is valid if:
+                - The entry is a press for any note but the first one
+            """
+            note_start_map_t = map_t[note_start_select]
+
+            dt = np.empty(map_t.shape[0]) * np.nan
+            dt[note_start_idx_ref[1:]] = note_start_map_t[1:] - note_start_map_t[:-1]
 
             return dt
 
         def __get_dt_dec():
             """
             Get the time passed since last time interval decrease (BPM increase)
+            Data that does not pertain to a valid entries is marked as np.nan
+            
+            Entries consist of score points related to press, release, etc. 
+            An entry is valid if:
+                - The entry is a press for any note
             """
-            dt_dec = np.zeros(map_t.shape[0])
+            note_start_map_t = map_t[note_start_select]
 
-            dt0 = map_t[1:-1] - map_t[:-2]   # t1 - t0
-            dt1 = map_t[2:] - map_t[1:-1]    # t2 - t1
+            dt0 = note_start_map_t[1:-1] - note_start_map_t[:-2]   # t1 - t0
+            dt1 = note_start_map_t[2:] - note_start_map_t[1:-1]    # t2 - t1
+
+            dt_dec = np.empty(map_t.shape[0]) * np.nan
+
+            # The first inverval decrease comes from lack of notes before the start of the map. 
+            # The time since last decrease for first note is ALWAYS 0
+            # The time since last decrease for the second note is ALWAYS the time between the first and second notes
+            dt_dec[note_start_idx_ref[0]] = 0
+            dt_dec[note_start_idx_ref[1]] = dt0[0]
+
+            ms = dt0[0]
 
             # How much the interval needs to change by to be considered a BPM increase
             # For example, if the previous interval is 100ms and the current is 50ms,
@@ -124,12 +154,6 @@ class DataProcessing():
             # On the other hand, if the previous interval is 100ms and the interval changes to 95ms, 
             # then the interval is not considered a BPM increase.
             d_threshold = 0.95  # Must be <= 1
-
-            # The first inverval decrease comes from lack of notes before the start of the map. 
-            # The time since last decrease for first note is ALWAYS 0
-            # The time since last decrease for the second note is ALWAYS the time between the first and second notes
-            dt_dec[1] = dt0[0]
-            ms = dt0[0]
 
             for i in range(dt0.shape[0]):
                 if dt1[i] < dt0[i]*d_threshold:
@@ -141,18 +165,31 @@ class DataProcessing():
                     # so the time interval to add is t2 - t1
                     ms += dt1[i]
 
-                dt_dec[i + 2] = ms
+                dt_dec[note_start_idx_ref[i + 2]] = ms
 
             return dt_dec
 
         def __get_dt_inc():
             """
             Get the time passed since last time interval increase (BPM decrease)
+            Data that does not pertain to a valid entries is marked as np.nan
+            
+            Entries consist of score points related to press, release, etc. 
+            An entry is valid if:
+                - The entry is a press for any note
             """
-            dt_inc = np.zeros(map_t.shape[0])
+            note_start_map_t = map_t[note_start_select]
 
-            dt0 = map_t[1:-1] - map_t[:-2]   # t1 - t0
-            dt1 = map_t[2:] - map_t[1:-1]    # t2 - t1
+            dt0 = note_start_map_t[1:-1] - note_start_map_t[:-2]   # t1 - t0
+            dt1 = note_start_map_t[2:] - note_start_map_t[1:-1]    # t2 - t1
+
+            dt_inc = np.empty(map_t.shape[0]) * np.nan
+
+            # The first inverval increase comes as soon as note t2 is further than expected
+            # This makes the time since last increase for first and second notes ALWAYS 0
+            dt_inc[note_start_idx_ref[0:2]] = 0
+
+            ms = 0
 
             # How much the interval needs to change by to be considered a BPM decrease
             # For example, if the previous interval is 100ms and the current is 200ms,
@@ -160,10 +197,6 @@ class DataProcessing():
             # On the other hand, if the previous interval is 100ms and the interval changes to 105ms, 
             # then the interval is not considered a BPM increase.
             d_threshold = 1.05  # Must be >= 1
-
-            # The first inverval increase comes as soon as note t2 is further than expected
-            # This makes the time since last increase for first and second notes ALWAYS 0
-            ms = 0
 
             for i in range(dt0.shape[0]):
                 if dt1[i] > dt0[i]*d_threshold:
@@ -177,31 +210,54 @@ class DataProcessing():
                     # so the time interval to add is t2 - t1
                     ms += dt1[i]
 
-                dt_inc[i + 2] = ms
+                dt_inc[note_start_idx_ref[i + 2]] = ms
 
             return dt_inc
             
         def __get_ds():
             """
             Gets the spacing between each note
+            Data that does not pertain to a valid entries is marked as np.nan
+            
+            Entries consist of score points related to press, release, etc. 
+            An entry is valid if:
+                - It is not an empty hit
+            Entries include:
+                - All scorepoints, regardless of whether they are press, release, or hold.
             """
-            dx = map_x[1:] - map_x[:-1]  # x1 - x0
-            dy = map_y[1:] - map_y[:-1]  # y1 - y0
+            not_empty_map_x = map_x[empty_filter]
+            not_empty_map_y = map_y[empty_filter]
 
-            ds = np.zeros(map_x.shape[0])
-            ds[1:] = (dx**2 + dy**2)**0.5
+            dx = not_empty_map_x[1:] - not_empty_map_x[:-1]  # x1 - x0
+            dy = not_empty_map_y[1:] - not_empty_map_y[:-1]  # y1 - y0
+
+            ds = np.empty(map_x.shape[0]) * np.nan
+            
+            # Cursor is assumed to start on first note, so distance from prev point is 0
+            ds[not_empty_idx_ref[0]] = 0
+            ds[not_empty_idx_ref[1:]] = (dx**2 + dy**2)**0.5
 
             return ds
 
         def __get_angles():
             """
             Gets the angle between each note
+            Data that does not pertain to a valid entries is marked as np.nan
+            
+            Entries consist of score points related to press, release, etc. 
+            An entry is valid if:
+                - It is not an empty hit
+                - It is not the first or last entry 
+                    (requires a point present before and after to calc angle of current point)
             """
-            dx0 = map_x[1:-1] - map_x[:-2]   # x1 - x0
-            dx1 = map_x[2:] - map_x[1:-1]    # x2 - x1
+            not_empty_map_x = map_x[empty_filter]
+            not_empty_map_y = map_y[empty_filter]
 
-            dy0 = map_y[1:-1] - map_y[:-2]   # y1 - y0
-            dy1 = map_y[2:] - map_y[1:-1]    # y2 - y1
+            dx0 = not_empty_map_x[1:-1] - not_empty_map_x[:-2]   # x1 - x0
+            dx1 = not_empty_map_x[2:] - not_empty_map_x[1:-1]    # x2 - x1
+
+            dy0 = not_empty_map_y[1:-1] - not_empty_map_y[:-2]   # y1 - y0
+            dy1 = not_empty_map_y[2:] - not_empty_map_y[1:-1]    # y2 - y1
             
             theta_d0 = np.arctan2(dy0, dx0)*(180/math.pi)
             theta_d1 = np.arctan2(dy1, dx1)*(180/math.pi)
@@ -210,10 +266,11 @@ class DataProcessing():
             thetas[thetas > 180] = 360 - thetas[thetas > 180]
             thetas = np.round(thetas)
 
-            angles = np.zeros(map_x.shape[0])
-            angles[0] = np.nan
-            angles[1:-1] = thetas
-            angles[-1] = np.nan
+            angles = np.empty(map_x.shape[0]) * np.nan
+            
+            # angles[0] = np.nan (implicit)
+            angles[not_empty_idx_ref[1:-1]] = thetas
+            # angles[-1] = np.nan (implicit)
 
             return angles
 
