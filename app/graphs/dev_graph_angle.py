60f12c9bf6ff428bda98f10c38729e8def6ad40d
abraker <abraker95@gmail.com>
2022-01-22 20:54:08 -0500

    Trash the chunk code as it's completely broken and hard to work with; Instead bin data per graph. Fixes binned data is now treated per-play instead of as one

--- app/graphs/dev_graph_angle.py
+++ app/graphs/dev_graph_angle.py
@@ -2,27 +2,25 @@ import pyqtgraph
 from pyqtgraph.Qt import QtGui
 import numpy as np
 
+from osu_analysis import StdScoreData
+from app.data_recording.data import RecData
 
 class DevGraphAngle(QtGui.QWidget):
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        self.CS     = 0
-        self.AR     = 1
-        self.DT     = 2
-        self.DT_INC = 3
-        self.DT_DEC = 4
-        self.DS     = 5
-        self.ANGLE  = 6
-        self.DEV_X  = 7
-        self.DEV_Y  = 8
-        self.DEV_T  = 9
-        self.AVG_X  = 10
-        self.AVG_Y  = 11
-        self.AVG_T  = 12
+        self.DEV_DATA_X = 0
+        self.DEV_DATA_Y = 1
+        self.DEV_DATA_T = 2
 
-        self.__dev_select = self.DEV_X
+        self.DEV_TYPE_AVG = 0
+        self.DEV_TYPE_DEV = 1
+
+        self.NEEDED_NUM_DATA_POINTS = 30
+
+        self.__dev_data_select = self.DEV_DATA_X
+        self.__dev_type_select = self.DEV_TYPE_DEV
         self.__avg_data_points = True
 
         # Main graph
@@ -53,14 +51,107 @@ class DevGraphAngle(QtGui.QWidget):
         self.__layout.addWidget(self.__graph)
 
 
-    def plot_data(self, data):
-        if data.shape[0] == 0:
-            return
+    def __get_deviation_data(self, play_data):
+        '''
+        x-axis: angles
+        y-axis: deviation or mean
+        color:  bpm
+
+        Meant to be used on single play and not multiple plays
+        '''
+        # Filters to get just hitcircles with valid hits
+        data_filter = np.ones(play_data.shape[0], dtype=bool)
+
+        # Filter out sliders
+        data_filter[:-1] = \
+            (play_data[:-1, RecData.ACT_TYPE] == StdScoreData.ACTION_PRESS) & ~(
+                (play_data[1:, RecData.ACT_TYPE] == StdScoreData.ACTION_HOLD) | \
+                (play_data[1:, RecData.ACT_TYPE] == StdScoreData.ACTION_RELEASE)
+            )
+
+        # Select hit presses
+        data_filter &= (play_data[:, RecData.HIT_TYPE] == StdScoreData.TYPE_HITP)
+
+        # Apply filter
+        play_data = play_data[data_filter]
+
+        # Gather relevant data
+        data_c = 15000/play_data[:, RecData.DT]
+        data_x = play_data[:, RecData.ANGLE]
+
+        if self.__dev_data_select == self.DEV_DATA_X:
+            data_y = play_data[:, RecData.X_OFFSETS]
+        elif self.__dev_data_select == self.DEV_DATA_Y:
+            data_y = play_data[:, RecData.Y_OFFSETS]
+        elif self.__dev_data_select == self.DEV_DATA_T:
+            data_y = play_data[:, RecData.T_OFFSETS]
+
+        #            MIN    MAX   MIN DELTA
+        chunks_c = [   0,  1000,   5   ]      # BPM,   200 bins max
+        chunks_x = [   0,   180,   3   ]      # Angle,  60 bins max
+
+        # Filter out data outside the range
+        range_filter = \
+            (chunks_c[0] <= data_c) & (data_c <= chunks_c[1]) & \
+            (chunks_x[0] <= data_x) & (data_x <= chunks_x[1])
+
+        data_c = data_c[range_filter]
+        data_x = data_x[range_filter]
+        data_y = data_y[range_filter]
+
+        # Reduce data to bins
+        num_bins_c = (chunks_c[1] - chunks_c[0])//chunks_c[2]
+        num_bins_x = (chunks_x[1] - chunks_x[0])//chunks_x[2]
+
+        dev_data_c = np.linspace(chunks_c[0], chunks_c[1], num_bins_c)
+        dev_data_x = np.linspace(chunks_x[0], chunks_x[1], num_bins_x)
+
+        idx_data_c = np.digitize(data_c, dev_data_c) - 1
+        idx_data_x = np.digitize(data_x, dev_data_x) - 1
+
+        c_unique_idxs = np.unique(idx_data_c)
+        x_unique_idxs = np.unique(idx_data_x)
+
+        dev_data = np.zeros((c_unique_idxs.shape[0]*x_unique_idxs.shape[0], 3), dtype=float)
+
+        for c_idx in range(c_unique_idxs.shape[0]):
+            for x_idx in range(x_unique_idxs.shape[0]):
+                data_select = (idx_data_c == c_unique_idxs[c_idx]) & (idx_data_x == x_unique_idxs[x_idx])
+                if np.sum(data_select) < self.NEEDED_NUM_DATA_POINTS:
+                    continue
+
+                if self.__dev_type_select == self.DEV_TYPE_AVG:
+                    dev_data_y = np.mean(data_y[data_select])
+                elif self.__dev_type_select == self.DEV_TYPE_DEV:
+                    dev_data_y = np.std(data_y[data_select])
+                else:
+                    print('Unknown deviation type')
+                    return
+
+                idx_dev_data = c_idx*x_unique_idxs.shape[0] + x_idx
+                dev_data[idx_dev_data, 0] = dev_data_y
+                dev_data[idx_dev_data, 1] = dev_data_x[x_unique_idxs[x_idx]]
+                dev_data[idx_dev_data, 2] = dev_data_c[c_unique_idxs[c_idx]]
+
+        return dev_data
+
+
+    def plot_data(self, play_data):
+        # Go through each play and fill in deviation data
+        dev_data = np.zeros(shape=(0, 3), dtype=float)
+
+        unique_timestamps = np.unique(play_data[:, RecData.TIMESTAMP])
+        for timestamp in unique_timestamps:
+            data = play_data[play_data[:, RecData.TIMESTAMP] == timestamp]
+            dev_data = np.insert(dev_data, 0, self.__get_deviation_data(data), axis=0)
 
         # Clear plots for redraw
         self.__graph.clearPlots()
 
-        bpm_data = 15000/data[:, self.DT]
+        if dev_data.shape[0] == 0:
+            return
+
+        bpm_data = dev_data[:, 2]
         unique_bpms = np.unique(bpm_data)
 
         bpm_lut = pyqtgraph.ColorMap(
@@ -77,46 +168,31 @@ class DevGraphAngle(QtGui.QWidget):
         # Main plot - deviation vs osu!px
         # Adds a plot for every unique BPM recorded
         for bpm in unique_bpms:
-            bpm_select = bpm_data == bpm
-            angles = data[bpm_select, self.ANGLE]
+            data_select = (bpm_data == bpm)
+            if not any(data_select):
+                # Selected region has no data. Nothing else to do
+                continue
 
-            # Determine data selected by osu!px
-            if self.__dev_select == self.DEV_X:
-                self.__graph.setTitle('Aim dev-x (angle)')
-                data_y = data[bpm_select, self.DEV_X]
-            elif self.__dev_select == self.DEV_Y:
-                self.__graph.setTitle('Aim dev-y (angle)')
-                data_y = data[bpm_select, self.DEV_Y]
-            elif self.__dev_select == self.DEV_T:
-                self.__graph.setTitle('Aim dev-t (angle)')
-                data_y = data[bpm_select, self.DEV_T]
-            elif self.__dev_select == self.AVG_X:
-                self.__graph.setTitle('Aim avg-x (bpm)')
-                data_y = data[bpm_select, self.AVG_X]
-            elif self.__dev_select == self.AVG_Y:
-                self.__graph.setTitle('Aim avg-y (bpm)')
-                data_y = data[bpm_select, self.AVG_Y]
-            elif self.__dev_select == self.AVG_T:
-                self.__graph.setTitle('Aim avg-t (bpm)')
-                data_y = data[bpm_select, self.AVG_T]
-                
+            data_y = dev_data[data_select, 0]
+            data_x = dev_data[data_select, 1]
+
             if self.__avg_data_points:
                 # Use best N points for data display
                 num_points = 10 # min(len(stdevs), self.MAX_NUM_DATA_POINTS)
 
                 # Average overlapping data points (those that fall on same angle)
-                data_y = np.asarray([ np.sort(data_y[angles == angle])[:num_points].mean() for angle in np.unique(angles) ])
-                angles = np.unique(angles)
+                data_y = np.asarray([ np.sort(data_y[data_x == x])[:num_points].mean() for x in np.unique(data_x) ])
+                unique_data_x = np.unique(data_x)
 
                 # Get sort mapping to make points on line graph connect in proper order
-                idx_sort = np.argsort(angles)
-                data_x = angles[idx_sort]
+                idx_sort = np.argsort(unique_data_x)
+                data_x = unique_data_x[idx_sort]
                 data_y = data_y[idx_sort]
-            else:
-                data_x = angles
 
-            # Draw plot
+            # Plot color
             color = bpm_lut.map(bpm, 'qcolor')
+
+            self.__graph.plot(x=data_x, y=data_y, symbol='o', symbolPen=None, symbolSize=5, pen=None, symbolBrush=color, name=f'{bpm:.2f} bpm')
 
             '''
             m, b = MathUtils.linear_regresion(angles, stdevs)
@@ -130,8 +206,6 @@ class DevGraphAngle(QtGui.QWidget):
             else:
                 self.__graph.plot(x=angles, y=stdevs, symbol='o', symbolPen=None, symbolSize=5, pen=None, symbolBrush=color, name=f'{bpm:.0f} bpm')
             '''
-
-            self.__graph.plot(x=data_x, y=data_y, symbol='o', symbolPen=None, symbolSize=5, pen=None, symbolBrush=color, name=f'{bpm:.2f} bpm')
 
 
     def set_dev(self, dev):
--- app/graphs/dev_graph_vel.py
+++ app/graphs/dev_graph_vel.py
@@ -5,28 +5,27 @@ from pyqtgraph.Qt import QtCore
 import math
 import numpy as np
 
+from osu_analysis import StdScoreData
+from app.data_recording.data import RecData
+
 
 class DevGraphVel(QtGui.QWidget):
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        self.CS     = 0
-        self.AR     = 1
-        self.DT     = 2
-        self.DT_INC = 3
-        self.DT_DEC = 4
-        self.DS     = 5
-        self.ANGLE  = 6
-        self.DEV_X  = 7
-        self.DEV_Y  = 8
-        self.DEV_T  = 9
-        self.AVG_X  = 10
-        self.AVG_Y  = 11
-        self.AVG_T  = 12
+        self.DEV_DATA_X = 0
+        self.DEV_DATA_Y = 1
+        self.DEV_DATA_T = 2
 
-        self.__dev_select = self.DEV_X
-        self.__avg_data_points = True
+        self.DEV_TYPE_AVG = 0
+        self.DEV_TYPE_DEV = 1
+
+        self.NEEDED_NUM_DATA_POINTS = 30
+
+        self.__dev_data_select = self.DEV_DATA_X
+        self.__dev_type_select = self.DEV_TYPE_DEV
+        self.__avg_data_points = False
 
         # Main graph
         self.__graph = pyqtgraph.PlotWidget(title='Aim dev-x (vel)')
@@ -36,8 +35,8 @@ class DevGraphVel(QtGui.QWidget):
         self.__graph.enableAutoRange(axis='y', enable=False)
         self.__graph.setLimits(xMin=0, xMax=5000, yMin=-10, yMax=200)
         self.__graph.setRange(xRange=[-10, 600], yRange=[-10, 20])
-        self.__graph.setLabel('left', 'deviation', units='σ', unitPrefix='')
-        self.__graph.setLabel('bottom', 'velocity', units='osu!px/ms', unitPrefix='')
+        self.__graph.setLabel('left', 'aim deviation', units='σ', unitPrefix='')
+        self.__graph.setLabel('bottom', 'velocity', units='osu!px/s', unitPrefix='')
         self.__graph.addLegend()
 
         # Deviation marker indicating expected deviation according to set CS
@@ -60,18 +59,114 @@ class DevGraphVel(QtGui.QWidget):
         self.__layout.setContentsMargins(0, 0, 0, 0)
         self.__layout.setSpacing(2)
         self.__layout.addWidget(self.__graph)
-        
 
-    def plot_data(self, data):
-        if data.shape[0] == 0:
-            return
+
+    def __get_deviation_data(self, play_data):
+        '''
+        x-axis: velocity
+        y-axis: deviation or mean
+        color:  angle
+
+        Meant to be used on single play and not multiple plays
+        '''
+        # Filters to get just hitcircles with valid hits
+        data_filter = np.ones(play_data.shape[0], dtype=bool)
+
+        # Filter out sliders
+        data_filter[:-1] = \
+            (play_data[:-1, RecData.ACT_TYPE] == StdScoreData.ACTION_PRESS) & ~(
+                (play_data[1:, RecData.ACT_TYPE] == StdScoreData.ACTION_HOLD) | \
+                (play_data[1:, RecData.ACT_TYPE] == StdScoreData.ACTION_RELEASE)
+            )
+
+        # Select hit presses
+        data_filter &= (play_data[:, RecData.HIT_TYPE] == StdScoreData.TYPE_HITP)
+
+        # Apply filter
+        play_data = play_data[data_filter]
+
+        # Gather relevant data
+        data_c = play_data[:, RecData.ANGLE]
+        data_x = 1000*play_data[:, RecData.DS]/play_data[:, RecData.DT]
+
+        if self.__dev_data_select == self.DEV_DATA_X:
+            data_y = play_data[:, RecData.X_OFFSETS]
+        elif self.__dev_data_select == self.DEV_DATA_Y:
+            data_y = play_data[:, RecData.Y_OFFSETS]
+        elif self.__dev_data_select == self.DEV_DATA_T:
+            data_y = play_data[:, RecData.T_OFFSETS]
+
+        #            MIN    MAX   MIN DELTA
+        chunks_c = [   0,   180,   3   ]     # Angle,   200 bins max
+        chunks_x = [   0,  2000,   10  ]     # Velocty,  60 bins max
+
+        # Filter out data outside the range
+        range_filter = \
+            (chunks_c[0] <= data_c) & (data_c <= chunks_c[1]) & \
+            (chunks_x[0] <= data_x) & (data_x <= chunks_x[1])
+
+        data_c = data_c[range_filter]
+        data_x = data_x[range_filter]
+        data_y = data_y[range_filter]
+
+        # Reduce data to bins
+        num_bins_c = (chunks_c[1] - chunks_c[0])//chunks_c[2]
+        num_bins_x = (chunks_x[1] - chunks_x[0])//chunks_x[2]
+
+        dev_data_c = np.linspace(chunks_c[0], chunks_c[1], num_bins_c)
+        dev_data_x = np.linspace(chunks_x[0], chunks_x[1], num_bins_x)
+
+        idx_data_c = np.digitize(data_c, dev_data_c) - 1
+        idx_data_x = np.digitize(data_x, dev_data_x) - 1
+
+        c_unique_idxs = np.unique(idx_data_c)
+        x_unique_idxs = np.unique(idx_data_x)
+
+        dev_data = np.zeros((c_unique_idxs.shape[0]*x_unique_idxs.shape[0], 3), dtype=float)
+
+        for c_idx in range(c_unique_idxs.shape[0]):
+            for x_idx in range(x_unique_idxs.shape[0]):
+                data_select = (idx_data_c == c_unique_idxs[c_idx]) & (idx_data_x == x_unique_idxs[x_idx])
+                if np.sum(data_select) < self.NEEDED_NUM_DATA_POINTS:
+                    continue
+
+                if self.__dev_type_select == self.DEV_TYPE_AVG:
+                    dev_data_y = np.mean(data_y[data_select])
+                elif self.__dev_type_select == self.DEV_TYPE_DEV:
+                    dev_data_y = np.std(data_y[data_select])
+                else:
+                    print('Unknown deviation type')
+                    return
+
+                idx_dev_data = c_idx*x_unique_idxs.shape[0] + x_idx
+                dev_data[idx_dev_data, 0] = dev_data_y
+                dev_data[idx_dev_data, 1] = dev_data_x[x_unique_idxs[x_idx]]
+                dev_data[idx_dev_data, 2] = dev_data_c[c_unique_idxs[c_idx]]
+
+        # Filter out ones that don't have enough data
+        dev_data = dev_data[dev_data[:, 0] != 0]
+
+        return dev_data
+
+
+    def plot_data(self, play_data):
+        # Go through each play and fill in deviation data
+        dev_data = np.zeros(shape=(0, 3), dtype=float)
+
+        unique_timestamps = np.unique(play_data[:, RecData.TIMESTAMP])
+        for timestamp in unique_timestamps:
+            data = play_data[play_data[:, RecData.TIMESTAMP] == timestamp]
+            dev_data = np.insert(dev_data, 0, self.__get_deviation_data(data), axis=0)
 
         # Clear plots for redraw
         self.__graph.clearPlots()
         self.__text.setText(f'')
 
+        if dev_data.shape[0] == 0:
+            return
+
         # Colored gradient r->g->b multiple plots at different angles
-        unique_angs = np.unique(data[:, self.ANGLE])
+        unique_angs = np.unique(dev_data[:, 2])
 
         angle_lut = pyqtgraph.ColorMap(
             np.linspace(min(unique_angs), max(unique_angs), 3),
@@ -87,41 +182,31 @@ class DevGraphVel(QtGui.QWidget):
         # Adds a plot for every unique BPM recorded
         for angle in unique_angs:
             # Determine data selected by angle
-            data_select = (data[:, self.ANGLE] == angle)
+            data_select = (dev_data[:, 2] == angle)
             if not any(data_select):
                 # Selected region has no data. Nothing else to do
                 continue
 
-            # Extract relavent data
-            if self.__dev_select == self.DEV_X:
-                self.__graph.setTitle('Aim dev-x (vel)')
-                stdevs = data[data_select, self.DEV_X]
-            elif self.__dev_select == self.DEV_Y:
-                self.__graph.setTitle('Aim dev-y (vel)')
-                stdevs = data[data_select, self.DEV_Y]
-            elif self.__dev_select == self.DEV_T:
-                self.__graph.setTitle('Aim dev-t (vel)')
-                stdevs = data[data_select, self.DEV_T]
-            elif self.__dev_select == self.AVG_X:
-                self.__graph.setTitle('Aim avg-x (bpm)')
-                stdevs = data[data_select, self.AVG_X]
-            elif self.__dev_select == self.AVG_Y:
-                self.__graph.setTitle('Aim avg-y (bpm)')
-                stdevs = data[data_select, self.AVG_Y]
-            elif self.__dev_select == self.AVG_T:
-                self.__graph.setTitle('Aim avg-t (bpm)')
-                stdevs = data[data_select, self.AVG_T]
+            data_y = dev_data[data_select, 0]
+            data_x = dev_data[data_select, 1]
 
-            pxs = data[data_select, self.DS]
-            dt = data[data_select, self.DT]
+            if self.__avg_data_points:
+                # Use best N points for data display
+                num_points = 10 # min(len(data_y), self.MAX_NUM_DATA_POINTS)
 
-            # Velocity
-            vels = pxs/dt
+                # Average overlapping data points (those that fall on same velocity)
+                data_y = np.asarray([ np.sort(data_y[data_x == x])[:num_points].mean() for x in np.unique(data_x) ])
+                unique_data_x = np.unique(data_x)
+
+                # Get sort mapping to make points on line graph connect in proper order
+                idx_sort = np.argsort(unique_data_x)
+                data_x = unique_data_x[idx_sort]
+                data_y = data_y[idx_sort]
 
             # Plot color
             color = angle_lut.map(angle, 'qcolor')
 
-            self.__graph.plot(x=vels, y=stdevs, pen=None, symbol='o', symbolPen=None, symbolSize=5, symbolBrush=color, name=f'∠={angle:.2f}')
+            self.__graph.plot(x=data_x, y=data_y, pen=None, symbol='o', symbolPen=None, symbolSize=5, symbolBrush=color, name=f'∠={angle:.2f}')
 
             '''
             # Calc linear regression
