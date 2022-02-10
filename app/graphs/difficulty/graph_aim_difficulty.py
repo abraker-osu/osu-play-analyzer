f6d61e9cacafe960d22e656cdf14a1ff370d60a6
abraker <abraker95@gmail.com>
2022-02-10 01:21:55 -0500

    Factor in CS into aim difficulty calc

--- app/graphs/difficulty/graph_aim_difficulty.py
+++ app/graphs/difficulty/graph_aim_difficulty.py
@@ -8,7 +8,7 @@ from pyqtgraph.Qt import QtGui, QtCore
 
 from osu_analysis import StdScoreData
 
-from app.misc.utils import MathUtils
+from app.misc.osu_utils import OsuUtils
 from app.data_recording.data import RecData
 
 
@@ -78,18 +78,29 @@ class GraphAimDifficulty(QtGui.QWidget):
             self.__calc_data_event.emit(data_stub, data_stub, data_stub)
             return
 
-        # Calculate data
+        # Sliders are selected based on x1-x2 being press-release or x2-x3 being press-release
+        # slider_select is first filled with data for x2-x3 because that has one less element than data being worked with
+        slider_select = np.zeros(play_data.shape[0] - 2, dtype=bool)
+        slider_select[:-1] = (play_data[3:, RecData.ACT_TYPE] == StdScoreData.ACTION_RELEASE)
+
+        slider_select = ( \
+            ((play_data[1:-1, RecData.ACT_TYPE] == StdScoreData.ACTION_PRESS) & (play_data[2:, RecData.ACT_TYPE] == StdScoreData.ACTION_RELEASE)) | \
+            ((play_data[2:, RecData.ACT_TYPE] == StdScoreData.ACTION_PRESS) & slider_select)   \
+        )
+
+        # Calculate data (x2 is considered current score point, x1 and x0 are previous score points)
+        cs_px = OsuUtils.cs_to_px(play_data[0, RecData.CS])
         pos_x = play_data[:, RecData.X_POS]
         pos_y = play_data[:, RecData.Y_POS]
         timing = play_data[:, RecData.TIMINGS]
         is_miss = (play_data[:, RecData.HIT_TYPE] == StdScoreData.TYPE_MISS)
-        
+
         dx0 = pos_x[1:-1] - pos_x[:-2]   # x1 - x0
         dx1 = pos_x[2:] - pos_x[1:-1]    # x2 - x1
 
         dy0 = pos_y[1:-1] - pos_y[:-2]   # y1 - y0
         dy1 = pos_y[2:] - pos_y[1:-1]    # y2 - y1
-        
+
         theta_d0 = np.arctan2(dy0, dx0)*(180/math.pi)
         theta_d1 = np.arctan2(dy1, dx1)*(180/math.pi)
 
@@ -97,18 +108,26 @@ class GraphAimDifficulty(QtGui.QWidget):
         angles[angles > 180] = 360 - angles[angles > 180]
         angles = np.round(angles)
 
-        is_miss = is_miss[2:]
         distances = np.sqrt(np.square(pos_x[2:] - pos_x[1:-1]) + np.square(pos_y[2:] - pos_y[1:-1]))
         velocities = distances / (timing[2:] - timing[1:-1])
         angle_factor = (1 + 2.5*np.exp(-0.026*angles))/(1 + 2.5)
+        
+        cs_factor = np.full_like(angle_factor, OsuUtils.cs_to_px(4)/cs_px)
+        cs_factor[slider_select] = (OsuUtils.cs_to_px(4)/(1.5*cs_px))
 
-        data_y = velocities*angle_factor*4
-        data_x = np.linspace(0, 1, data_y.shape[0])
+        data_y = (cs_factor*velocities*angle_factor*4)
+        is_miss = is_miss[2:]
+        
+        if True:
+            data_x = np.linspace(0, 1, data_y.shape[0])
 
-        sort_idx = np.argsort(data_y)
-        data_y  = data_y[sort_idx]
-        is_miss = is_miss[sort_idx]
-
+            sort_idx = np.argsort(data_y)
+            data_y  = data_y[sort_idx]
+            is_miss = is_miss[sort_idx]
+        else:
+            # Debug
+            data_x = timing[2:]
+        
         self.__calc_data_event.emit(data_x, data_y, is_miss)
 
 
@@ -126,7 +145,7 @@ class GraphAimDifficulty(QtGui.QWidget):
         self.__diff_plot_hit.setData(x=data_x_hit, y=data_y_hit/2, top=data_y_hit/2, bottom=data_y_hit/2, pen=mkPen((200, 200, 200, 100), width=2))
         self.__diff_plot_miss.setData(x=data_x_miss, y=data_y_miss/2, top=data_y_miss/2, bottom=data_y_miss/2, pen=mkPen((200, 0, 0, 100), width=2))
 
-        self.__graph.setLimits(xMin=xMin, xMax=xMax)
+        #self.__graph.setLimits(xMin=xMin, xMax=xMax)
         self.__graph.setRange(xRange=[ xMin, xMax ])
 
         play_percent = 1 - data_y_miss.shape[0]/data_y.shape[0]
