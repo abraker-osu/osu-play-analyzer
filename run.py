2958110927c5b6017765082e3ff826eef7ee94b2
abraker <abraker95@gmail.com>
2022-05-19 15:42:02 -0400

    Guard imports behind __main__ check

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-import sys
+if __name__ == '__main__':
+    import sys
 
-from app.misc.Logger import Logger
+    from app.misc.Logger import Logger
 
-
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+    from PyQt5.QtCore import *
+    from PyQt5.QtWidgets import *
+    from PyQt5.QtGui import *
 
     from app.app import App
 
+    
 
-if __name__ == '__main__':
     Logger.get_logger('core').debug('Starting app')
 
     app = QApplication(sys.argv)
