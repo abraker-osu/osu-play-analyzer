80d25c2ae12be988f47f829f07e6cf9742bada7d
abraker95 <abraker95@gmail.com>
2024-12-15 18:04:43 -0500

    change reqs to point to main branches; add setup to readme; when building ensure the build generates fresh exe

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,11 @@
 @echo off
 
+echo Removing "build/..."
+rd /s /q build
+
+echo Removing "dist/..."
+rd /s /q dist
+
 python -O -m PyInstaller -n osu-performance-analyzer ^
     --collect-binaries=tables ^
     --onefile ^
