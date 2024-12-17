32d5226541ffd44e6a3c7bc6352264d980d48088
abraker95 <abraker95@gmail.com>
2024-12-17 16:36:10 -0500

    Fix new process being spawned when opening a *.h5 file
    the root reason is due to pandas trying to import tables lib via importlib, and importlib does something weird that creates a process fork when trying to resolve imports

--- src/run.py
+++ src/run.py
@@ -1,7 +1,14 @@
+import sys
+import multiprocessing
+
+
 # Everything is put under __main__ so that the map architect
 #  could load code without invoking the GUI
 if __name__ == '__main__':
-    import sys
+    if sys.platform.startswith('win'):
+        # See: https://stackoverflow.com/a/27694505
+        multiprocessing.freeze_support()
+
     from PyQt5 import QtWidgets
 
     from misc.Logger import Logger
