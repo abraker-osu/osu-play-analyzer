5a2ce3b9907e14994994f3a54bb1d68ceba01c51
abraker95 <abraker95@gmail.com>
2024-12-19 18:21:48 -0500

    special handling for windows

--- src/run.py
+++ src/run.py
@@ -1,13 +1,20 @@
-import sys
-import multiprocessing
-
-
 # Everything is put under __main__ so that the map architect
 #  could load code without invoking the GUI
 if __name__ == '__main__':
     if sys.platform.startswith('win'):
+    import os, sys
+    import multiprocessing
+
+    is_win = sys.platform.startswith('win')
+    is_exe = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+
+    if is_win and is_exe:
         # See: https://stackoverflow.com/a/27694505
         multiprocessing.freeze_support()
+
+    if not is_exe:
+        # Add editable libs to path
+        sys.path.append(f'{os.environ["VIRTUAL_ENV"]}{os.sep}src')
 
     from PyQt6 import QtWidgets
 
