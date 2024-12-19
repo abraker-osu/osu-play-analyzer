01c336b41395696f8fef0d453e74e4dafbef0b70
abraker95 <abraker95@gmail.com>
2024-12-19 18:22:44 -0500

    cleanup; ensure needed packages are installed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-# Check if python3 is installed
-if ! command -v python3 &> /dev/null; then
-    echo "python3 is not installed. Installing..."
-    sudo apt-get install python3
+# Needed for pyqt6
+sudo apt install -y libxkbcommon-x11-0 libxcb-cursor-dev libxcb-icccm4 libxcb-keysyms1
 
-    if [ $? -ne 0 ]; then
-        echo "Failed to install python3"
-        exit 1
-    fi
-fi
+# Python prereqs
+sudo apt install -y python3
+sudo apt install -y python3-venv
 
-# Check if python3-venv is installed
-if ! python3 -c "import ensurepip" &> /dev/null; then
-    echo "python3-venv is not installed. Installing..."
-    sudo apt-get install python3-venv
-
-    if [ $? -ne 0 ]; then
-        echo "Failed to install python3-venv"
-        exit 1
-    fi
-fi
-
+# Create venv
 if [ ! -d "venv_nix/bin" ]; then
     echo "No venv found. Creating..."
     python3 -m venv venv_nix
@@ -63,23 +49,5 @@ if [ $? -ne 0 ]; then
     echo "Failed to fix src paths"
     exit 1
 fi
-
-# # Get the Python version (e.g., 3.8 or 3.9)
-# PY_VER=$(python3 --version | awk '{print $2}' | cut -d '.' -f 1-2)
-
-# # Set the hook file path
-# hook_file="$VIRTUAL_ENV/lib/python$PY_VER/site-packages/sitecustomize.py"
-# rm -f "$hook_file"
-
-# # Create a 'sitecustomize.py' file
-# # Python automatically runs this before importing any modules
-# # This edits paths to allow module import from workspace root
-# echo "import sys" > "$hook_file"
-# echo "sys.path.insert(0, f'{sys.prefix}/src')" >> "$hook_file"
-
-# if [ ! -f "$hook_file" ]; then
-#     echo "Failed to create sitecustomize.py"
-#     exit 1
-# fi
 
 echo "[ DONE ]"
