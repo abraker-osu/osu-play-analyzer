759d1ccf07c4400fbdf4792256b98cd56e46b1af
abraker95 <abraker95@gmail.com>
2024-12-22 13:50:50 -0500

    add venv/src to path in build.spec and run.py instead of relying on sitecustomize.py; gets linux build binary to work

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -42,35 +42,4 @@ if %ERRORLEVEL% GEQ 1 (
     EXIT /B 1
 )
 
-:: When pip installs editable modules via setuptools, it applies PEP 503
-:: (https://peps.python.org/pep-0503/#normalized-names). This requires modules'
-:: names to be "normalized" form for web/system compatibility. Unfortunately,
-:: setuptools applies this to the module directory as well, causing python
-:: imports to break. Setuptools fixes this via the __editable__.* python hooks
-:: in venv, which depends on specific directories contiaining dashes. As such,
-:: the renaming above breaks python imports. This is solved by creating another
-:: python hook below to fix the search paths.
-::
-:: It is possible for sys.path to be modified in run.py, but that only solves
-:: the issue when running from source. When running pyinstaller it unfortunately
-:: doesn't see the libraries in the venv/src directory unless this is done from
-:: sitecustomize.py.
-::
-:: NOTE: Assumes nothing else generates sitecustomize.py
-set hook_file=%VIRTUAL_ENV%\Lib\site-packages\sitecustomize.py
-del /s /Q "%hook_file%" >nul 2>&1
-
-:: Create a 'sitecustomize.py' file
-:: Python automatically runs this before importing any modules
-:: This edits paths to allow module import from workspace root
-::
-:: NOTE: '^' is used to escape the parenthesis
-echo import sys > "%hook_file%"
-echo sys.path.insert^(0, f'{sys.prefix}\\src'^) >> "%hook_file%"
-
-if NOT EXIST "%hook_file%" (
-    echo Failed to create sitecustomize.py
-    EXIT /B 1
-)
-
 echo [ DONE ]
